a477b5a6609a6fc0b9c2bc8a19643d276bfc506c
atribolt <atribolt@gmail.com>
2026-03-29 14:01:28 +0300

    Сборка страницы
    - поправил путь до исходников
    - добавил создание каталогов

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,12 +10,14 @@ if ! npx --version >/dev/null 2>&1 ; then
   exit 1
 fi
 
-SRC=$(realpath $(dirname "$0/.."))
+SRC=$(realpath $(dirname "$0")/..)
 TARGET_DIR="${TARGET_DIR}"
 BUILD_DIR="${BUILD_DIR}"
 
 test -n "${TRAGET_DIR}" || TARGET_DIR="${SRC}/dist"
 test -n "${BUILD_DIR}" || BUILD_DIR="${SRC}/build"
+
+mkdir -p "${TARGET_DIR}" "${BUILD_DIR}"
 
 pushd "${BUILD_DIR}"
 npm install gramax-cli
